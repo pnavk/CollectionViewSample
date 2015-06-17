674b0037fc6c6a90324bd72d0834d039a1bcad5b
Pranav <pranav@bluetubes-MacBook-Pro.local>
2015-06-17 15:30:52 -0400

    Initial Commit

---
+++ CollectionViewApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewApp
+//
+//  Created by Pranav on 6/17/15.
+//  Copyright (c) 2015 Pranav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CollectionViewAppTests/CollectionViewAppTests.m
@@ -0,0 +1,40 @@
+//
+//  CollectionViewAppTests.m
+//  CollectionViewAppTests
+//
+//  Created by Pranav on 6/17/15.
+//  Copyright (c) 2015 Pranav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CollectionViewAppTests : XCTestCase
+
+@end
+
+@implementation CollectionViewAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
